dcf488c46b5e8c4d36903bab6c9b526798fb17f6
Earl Harris <earleharrisjr@gmail.com>
2023-01-17 17:56:14 -0500

    created meme object using react useState, configure code to display image again from the object

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,25 +2,36 @@ import React from "react";
 import memesData from "../memesData";
 
 export default function Meme() {
-    const [memeImage, setMemeImage] = React.useState("");
+    const [meme, setMeme] = React.useState({
+        topText: "Top text",
+        bottomText: "Bottom text",
+        randomImage: "http://i.imgflip.com/1bij.jpg",
+    });
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
 
     function getMemeImage() {
-        const memesArray = memesData.data.memes;
+        const memesArray = allMemeImages.data.memes;
         const randomNumber = Math.floor(Math.random() * memesArray.length);
-        setMemeImage(memesArray[randomNumber].url)
-    }
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+    };
 
     return (
         <main>
             <div className="form--meme">
                 <input type="text"
                     id="top"
-                    placeholder="shut up"
+                    placeholder={meme.topText}
                     className="form--input">
                 </input>
                 <input type="text"
                     id="bottom"
-                    placeholder="and take my money"
+                    placeholder={meme.bottomText}
                     className="form--input">
                 </input>
                 <button type="submit"
@@ -30,7 +41,7 @@ export default function Meme() {
                     Get a new meme image 🖼
                 </button>
             </div>
-            <img src={memeImage} className="meme--image" />
+            <img src={meme.randomImage} className="meme--image" />
 
         </main>
     )
